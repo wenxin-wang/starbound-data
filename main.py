f2c925f207de2b2560629db204f33697ccc3c9a0
Wenxin Wang <wenxin.wang94@gmail.com>
2017-02-07 23:32:20 +0800

    fix categorymember continue, add redirection support

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ import wiki
 
 async def main(loop):
     async with aiohttp.ClientSession(loop=loop) as session:
-        #async for u, t in wiki.get_crops_time(session):
-        #    print(u, t)
-        #return await wiki.get_item(session, 'B', '/Bolt_O\'s')
-        #return await wiki.get_item(session, 'B', '/Tropical_Punch')
-        #return await wiki.get_food_in_category(session, 'Prepared Food')
+        #return await wiki.get_crops_time(session)
+        #return await wiki.get_item(session, '/Cotton')
+        #return await wiki.get_item(session, '/Tropical_Punch')
+        #return await wiki.get_food_in_category(session, 'Food')
 
-        return await wiki.get_food_all_categories(session)
+        #return await wiki.get_redirect(session, '/Cotton')
+        return await wiki.gen_food_data(session)
 
 
 if __name__ == '__main__':
     loop = aio.get_event_loop()
-    foods = loop.run_until_complete(main(loop))
+    food = loop.run_until_complete(main(loop))
     with open('food.json', 'w') as fd:
-        json.dump(foods, fd)
+        json.dump(food, fd)
--- wiki.py
+++ wiki.py
@@ -3,31 +3,58 @@ import gzip
 import asyncio as aio
 from yarl import URL
 # yarl seems to have bug in quoting, so use urllib's quote instead
-from urllib.parse import quote
+from urllib.parse import quote, unquote
 from bs4 import BeautifulSoup, SoupStrainer
 
 base = 'http://starbounder.org'
 api = base + '/mediawiki/api.php'
 
-crops = '/Crops'
+crops_page = '/Crops'
 category = '/Category:'
 category_food = category + 'Food'
-food_categories = ['Food', 'Prepared_Food', 'Drink']
+food_categories = ['Food', 'Prepared_Food', 'Drink', 'Cooking Ingredient']
+
+redirected = {}
+
+
+def get_real_url(content, u):
+    parse_only = SoupStrainer(id='ca-nstab-main')
+    soup = BeautifulSoup(content, 'html.parser',
+                         parse_only=parse_only)
+    _u = unquote(soup.find('a')['href'])
+    if u != _u:
+        redirected[u] = _u
+        return _u
+
+
+async def get_content(session, url):
+    async with session.get(URL(base + quote(url), encoded=True)) as res:
+        return await res.text()
+
+
+async def get_redirect(session, url):
+    url = unquote(url)
+    u = redirected.get(url)
+    if u:
+        return u
+    content = await get_content(session, url)
+    print("CHECK REDIRECT %s" % url)
+    return get_real_url(content, url)
 
 
 async def url_soup(session, url, parse_only=None):
-    async with session.get(URL(base + quote(url), encoded=True)) as res:
-        content = await res.text()
-        print("GET %s" % url)
-        soup = BeautifulSoup(content, 'html.parser',
-                             parse_only=parse_only)
-        return soup
+    url = unquote(url)
+    content = await get_content(session, url)
+    print("GET %s" % url)
+    soup = BeautifulSoup(content, 'html.parser',
+                         parse_only=parse_only)
+    return soup, get_real_url(content, url) or url
 
 
-async def get_item(session, name, url):
+async def get_item(session, url):
     parse_only = SoupStrainer('div', class_='infoboxwrapper')
-    soup = await url_soup(session, url, parse_only)
-    item = {'name': name}
+    soup, url = await url_soup(session, url, parse_only)
+    item = {'name': url[1:], 'url': url}
     pixel_a = soup.find('a', href='/Pixel')
     item['price'] = int(pixel_a.next_sibling.string)
     ing_text_div = soup.find('div', text='INGREDIENTS')
@@ -43,40 +70,48 @@ async def get_item(session, name, url):
     return item
 
 
-async def get_category_members(session, category):
+async def get_category_members(session, category, limit=50):
     params = {'action': 'query', 'list': 'categorymembers', 'continue': '',
-              'format': 'json', 'cmprop': 'title', 'cmlimit': '500', 'cmtitle':
+              'format': 'json', 'cmprop': 'title', 'cmlimit': str(limit), 'cmtitle':
               'Category:' + category}
-    async with session.get(api, params=params) as res:
-        data = await res.json()
-        print("LIST %s" % category)
-        for member in data['query']['categorymembers']:
-            title = member['title']
-            yield title, '/' + title.replace(' ', '_')
+    while True:
+        async with session.get(api, params=params) as res:
+            data = await res.json()
+            print("LIST %s" % category)
+            for member in data['query']['categorymembers']:
+                title = member['title']
+                yield title, '/' + title.replace(' ', '_')
+            cont = data.get('continue')
+            if not cont:
+                break
+            params['cmcontinue'] = cont['cmcontinue']
 
 
-async def get_crops_time(session, url=crops):
+async def get_crops_time(session, url=crops_page):
     harvest_re = re.compile(r"^\s*Harvest:\s*([0-9]+)\s*minutes\s*", re.I)
     parse_only = SoupStrainer(id='mw-content-text')
-    soup = await url_soup(session, url, parse_only)
+    soup, _ = await url_soup(session, url, parse_only)
     tag = soup.find(id='Crop_Images_and_Average_Growth_Times')
     while tag.name != 'table':
         tag = tag.next_element
+    times = {}
     for table in tag.find_all('table', class_='gametable'):
         a = table.find('a')
         p = table.find('p')
-        yield a['href'], harvest_re.match(p.string).group(1)
+        times[a['href']] = harvest_re.match(p.string).group(1)
+    return times
 
 
 async def get_food_in_category(session, category):
-    urls = []
     pending = []
     async for name, url in get_category_members(session, category):
-        urls.append(url)
-        pending.append(aio.ensure_future(get_item(session, name, url)))
+        pending.append(aio.ensure_future(get_item(session, url)))
     done, _ = await aio.wait(pending, loop=session.loop)
-    foods = {u:await f for u, f in zip(urls, done)}
-    return foods
+    food = {}
+    for d in done:
+        f = await d
+        food[f['url']] = f
+    return food
 
 
 async def get_food_all_categories(session, categories=food_categories):
@@ -88,21 +123,22 @@ async def get_food_all_categories(session, categories=food_categories):
     return res
 
 
-async def get_foods(session, url=category_food):
+async def gen_food_data(session):
     '''
-    Get a list of all kind of foods
+    Get a list of all kind of food
     Get harvest time for crops
     Try to get simple ingredients since ingredients may be compound
     Return a dictionary from food url to food dictionary
     '''
-    urls = []
-    pending = []
-    for div in soup.children:
-        for li in div.ul.find_all('li', recursive=False):
-            a = li.a
-            u = a['href']
-            urls.append(u)
-            pending.append(get_item(session, a.string, u))
-    done, _ = await aio.wait(pending, loop=session.loop)
-    foods = {u:await f for u, f in zip(urls, done)}
-    return foods
+    food, time = await aio.gather(get_food_all_categories(session),
+                                  get_crops_time(session), loop=session.loop)
+    for u, t in time.items():
+        try:
+            f = food[u]
+        except KeyError as e:
+            u = await get_redirect(session, u)
+            if not u:
+                raise e
+            f = food[u]
+        f['time'] = t
+    return food
